798f98d4b08521c92fde44ad1e1876f0a90b7688
Ich17 <andreasgeyer1@gmx.de>
2021-06-03 17:17:31 +0200

    Delete venv directory

--- venv/lib/python3.9/site-packages/skmultilearn/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Multi-label classification module for Python
-============================================
-
-Scikit-multilearn is a BSD-licensed library for multi-label classification that is
-built on top of the well-known scikit-learn ecosystem.
-
-"""
