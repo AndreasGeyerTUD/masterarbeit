f32c838bb186577056abf1f98b148b9bb3acc639
Andreas Geyer <andreasgeyer1@gmx.de>
2021-06-14 15:51:22 +0200

    - updated multilabel-results -> this needs some thinking on why it is this bad - heatmap -> added different resolutions - heatmap -> updated plots (without showing the plot it wasn't closed and there were overlays)

--- quality_metric/heatmap.py
+++ quality_metric/heatmap.py
@@ -7,13 +7,15 @@ from sklearn.datasets import make_classification
 from tqdm import tqdm
 
 
-def _round(matrix: np.array):
-    return (np.round(matrix, 4) * 1000).astype(int)
+def _round(matrix: np.array, res: int):
+    return (np.round(matrix, len(str(res))) * res).astype(int)
 
 
 def surface_plot(data, title: str, file_path: str, cmap: str = None):
     x = range(data.shape[0])
     y = range(data.shape[1])
+
+    fig = plt.figure()
 
     ax = plt.axes(projection='3d')
 
@@ -25,7 +27,8 @@ def surface_plot(data, title: str, file_path: str, cmap: str = None):
 
     plt.title(title)
     plt.savefig("{}_{}.pdf".format(file_path, cmap), format="pdf")
-    plt.show()
+    # plt.show()
+    plt.clf()
 
 
 def heatmap_plot(data, title: str, file_path: str, cmap: str = None):
@@ -33,51 +36,55 @@ def heatmap_plot(data, title: str, file_path: str, cmap: str = None):
 
     plt.imshow(data, cmap=cmap, interpolation='nearest')
     plt.colorbar()
+    plt.axis('off')
     ax.set_title(title)
     ax.axes.xaxis.set_visible(False)
     ax.axes.yaxis.set_visible(False)
     plt.savefig("{}_{}.pdf".format(file_path, cmap), format="pdf")
-    plt.show()
+    # plt.show()
+    plt.clf()
 
 
 def _main(save_dir: str):
-    Path(save_dir).mkdir(parents=True, exist_ok=True)
+    for res in [10, 100, 1000, 10000]:
+        cwd = save_dir + "/" + str(res)
+        Path(cwd).mkdir(parents=True, exist_ok=True)
 
-    results = np.zeros((1001, 1001)).astype(int)
-    label_1 = np.zeros((1001, 1001)).astype(int)
-    label_2 = np.zeros((1001, 1001)).astype(int)
+        results = np.zeros((res + 1, res + 1)).astype(int)
+        label_1 = np.zeros((res + 1, res + 1)).astype(int)
+        label_2 = np.zeros((res + 1, res + 1)).astype(int)
 
-    for i in tqdm(range(10000)):
-        x, y = make_classification(n_samples=10000, n_features=2, n_informative=2, n_redundant=0,
-                                   n_clusters_per_class=1,
-                                   random_state=i)
+        for i in tqdm(range(res * 10)):
+            x, y = make_classification(n_samples=res * 10, n_features=2, n_informative=2, n_redundant=0,
+                                       n_clusters_per_class=1,
+                                       random_state=i)
 
-        x_round = _round((x - x.min(0)) / x.ptp(0))
+            x_round = _round((x - x.min(0)) / x.ptp(0), res)
 
-        data = np.concatenate([x_round, y[:, None]], axis=1)
+            data = np.concatenate([x_round, y[:, None]], axis=1)
 
-        for x_1, x_2, y in data:
-            results[x_1][x_2] += 1
+            for x_1, x_2, y in data:
+                results[x_1][x_2] += 1
 
-            if y == 0:
-                label_1[x_1][x_2] += 1
-            if y == 1:
-                label_2[x_1][x_2] += 1
+                if y == 0:
+                    label_1[x_1][x_2] += 1
+                if y == 1:
+                    label_2[x_1][x_2] += 1
 
-    pd = "Points Distribution"
-    hm = "Heatmap"
-    sp = "Surface-Plot"
+        pd = "Points Distribution"
+        hm = "Heatmap"
+        sp = "Surface-Plot"
 
-    heatmap_plot(results, "{} - {}".format(pd, hm), "{}/heatmap".format(save_dir), "gist_heat")
-    heatmap_plot(results, "{} - {}".format(pd, hm), "{}/heatmap".format(save_dir), "tab20c_r")
-    heatmap_plot(label_1, "{} - {} - Label 0".format(pd, hm), "{}/heatmap_label_0".format(save_dir), "gist_heat")
-    heatmap_plot(label_1, "{} - {} - Label 0".format(pd, hm), "{}/heatmap_label_0".format(save_dir), "tab20c_r")
-    heatmap_plot(label_2, "{} - {} - Label 1".format(pd, hm), "{}/heatmap_label_1".format(save_dir), "gist_heat")
-    heatmap_plot(label_2, "{} - {} - Label 1".format(pd, hm), "{}/heatmap_label_1".format(save_dir), "tab20c_r")
+        heatmap_plot(results, "{} - {}".format(pd, hm), "{}/heatmap".format(cwd), "gist_heat")
+        heatmap_plot(results, "{} - {}".format(pd, hm), "{}/heatmap".format(cwd), "tab20c_r")
+        heatmap_plot(label_1, "{} - {} - Label 0".format(pd, hm), "{}/heatmap_label_0".format(cwd), "gist_heat")
+        heatmap_plot(label_1, "{} - {} - Label 0".format(pd, hm), "{}/heatmap_label_0".format(cwd), "tab20c_r")
+        heatmap_plot(label_2, "{} - {} - Label 1".format(pd, hm), "{}/heatmap_label_1".format(cwd), "gist_heat")
+        heatmap_plot(label_2, "{} - {} - Label 1".format(pd, hm), "{}/heatmap_label_1".format(cwd), "tab20c_r")
 
-    surface_plot(results, "{} - {}".format(sp, pd), "{}/surface_plot".format(save_dir), "RdYlGn_r")
-    surface_plot(label_1, "{} - {} - Label 0".format(sp, pd), "{}/surface_plot_label_0".format(save_dir), "RdYlGn_r")
-    surface_plot(label_2, "{} - {} - Label 1".format(sp, pd), "{}/surface_plot_label_1".format(save_dir), "RdYlGn_r")
+        surface_plot(results, "{} - {}".format(sp, pd), "{}/surface_plot".format(cwd), "RdYlGn_r")
+        surface_plot(label_1, "{} - {} - Label 0".format(sp, pd), "{}/surface_plot_label_0".format(cwd), "RdYlGn_r")
+        surface_plot(label_2, "{} - {} - Label 1".format(sp, pd), "{}/surface_plot_label_1".format(cwd), "RdYlGn_r")
 
 
 if __name__ == "__main__":
