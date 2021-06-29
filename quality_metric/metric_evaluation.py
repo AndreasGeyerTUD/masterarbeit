ca70d7268306c268278314d3ee2e59d922e89394
Andreas-Laptop <andreasgeyer1@gmx.de>
2021-06-29 14:19:22 +0200

    - wip updated evaluation to distinguish between singlelabel and multilabel

--- quality_metric/metric_evaluation.py
+++ quality_metric/metric_evaluation.py
@@ -4,13 +4,10 @@ import numpy as np
 import json
 
 
-def _main(result_path: str, save_path:str):
-    with open(result_path, "r") as file:
-        data = json.load(file)
-
+def singlelabel(data: dict) -> dict:
     helper = {}
 
-    for _, date in data.items():
+    for date in data.values():
         for entry, value in date.items():
             if entry not in helper:
                 helper[entry] = {}
@@ -31,6 +28,55 @@ def _main(result_path: str, save_path:str):
             if k == "best_params" or k == "opt_score": continue
             evaluation[key][k] = np.mean(v)
 
+    return evaluation
+
+
+def multilabel(data: dict) -> dict:
+    helper = {}
+
+    for date in data.values():
+        for method, value in date.items():
+            if method not in helper:
+                helper[method] = {}
+            if method == "ml_knn":
+                for met, val in value.items():
+                    if met not in helper[method]:
+                        helper[method][met] = {}
+                    for k, v in val.items():
+                        if v is None: continue
+                        if k not in helper[method]:
+                            helper[method][met][k] = [v]
+                        else:
+                            helper[method][met][k].append(v)
+            else:
+                for cla, val in value.items():
+                    if val is None: continue
+                    if cla not in helper[method]:
+                        helper[method][cla] = [val]
+                    else:
+                        helper[method][cla].append(val)
+
+    evaluation = {}
+
+
+    return data
+
+
+def _main(result_path: str, save_path: str):
+    with open(result_path, "r") as file:
+        data = json.load(file)
+
+    type = data["type"]
+
+    data.pop("type", None)
+
+    if type == "singlelabel":
+        evaluation = singlelabel(data)
+    elif type == "multilabel":
+        evaluation = multilabel(data)
+    else:
+        evaluation = {}
+
     evaluation["dataset_information"] = data["0"]["dataset_information"]
     evaluation["number_of_datasets"] = len(data)
 
@@ -40,9 +86,10 @@ def _main(result_path: str, save_path:str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', "--result_path", action='store', type=str, default="singlelabel_test_dataset/results.json",
+    parser.add_argument('-r', "--result_path", action='store', type=str,
+                        default="multilabel_test_dataset/results.json",
                         help='Where the json which should be evaluated is saved.')
-    parser.add_argument('-d', "--directory", action='store', type=str, default="singlelabel_test_dataset",
+    parser.add_argument('-d', "--directory", action='store', type=str, default="multilabel_test_dataset",
                         help='Dictionary where to evaluation results.')
 
     args = parser.parse_args()
