4e0af7f631591018e931e808f96a419731652a63
Andreas-Laptop <andreasgeyer1@gmx.de>
2021-06-30 10:02:15 +0200

    - same evaluation for multilabel and singlelabel - added possibility for boxplots of the results

--- quality_metric/metric_evaluation.py
+++ quality_metric/metric_evaluation.py
@@ -1,83 +1,72 @@
 import argparse
+import json
+from pathlib import Path
 
 import numpy as np
-import json
+from matplotlib import pyplot as plt
 
 
-def singlelabel(data: dict) -> dict:
+def evaluate(data: dict, dir_name: str, plot: bool = False) -> dict:
     helper = {}
 
     for date in data.values():
-        for entry, value in date.items():
-            if entry not in helper:
-                helper[entry] = {}
-            for key, val in value.items():
-                if val is None: continue
-                if key not in helper[entry]:
-                    helper[entry][key] = [val]
+        for key, value in date.items():
+            if key == "dataset_information": continue
+            if key not in helper:
+                helper[key] = {}
+            for k, v in value.items():
+                if v is None: continue
+                if k == "opt_score" or k == "best_params": continue
+                if k not in helper[key]:
+                    helper[key][k] = [v]
                 else:
-                    helper[entry][key].append(val)
+                    helper[key][k].append(v)
 
     evaluation = {}
 
     for key, value in helper.items():
-        if key == "dataset_information": continue
         if key not in evaluation:
             evaluation[key] = {}
         for k, v in value.items():
-            if k == "best_params" or k == "opt_score": continue
-            evaluation[key][k] = np.mean(v)
+            if plot: plot_boxplot(v, dir_name, "{} {}".format(key, k))
+            evaluation[key][k] = {"mean": np.mean(v),
+                                  "25th": np.quantile(v, 0.25),
+                                  "75th": np.quantile(v, 0.75),
+                                  "min": min(v),
+                                  "min_ind": int(np.argmin(v)),
+                                  "max": max(v),
+                                  "max_ind": int(np.argmax(v))}
 
     return evaluation
 
 
-def multilabel(data: dict) -> dict:
-    helper = {}
+def plot_boxplot(data, dir_name: str, title: str):
+    Path("{}/plots".format(dir_name)).mkdir(parents=True, exist_ok=True)
 
-    for date in data.values():
-        for method, value in date.items():
-            if method not in helper:
-                helper[method] = {}
-            if method == "ml_knn":
-                for met, val in value.items():
-                    if met not in helper[method]:
-                        helper[method][met] = {}
-                    for k, v in val.items():
-                        if v is None: continue
-                        if k not in helper[method]:
-                            helper[method][met][k] = [v]
-                        else:
-                            helper[method][met][k].append(v)
-            else:
-                for cla, val in value.items():
-                    if val is None: continue
-                    if cla not in helper[method]:
-                        helper[method][cla] = [val]
-                    else:
-                        helper[method][cla].append(val)
+    fig, ax = plt.subplots()
+    ax.set_title(title)
+    ax.boxplot(data)
+    ax.set_xticks([])
 
-    evaluation = {}
+    title = title.replace(" ", "_")
+
+    plt.savefig("{}/plots/{}.pdf".format(dir_name, title), format="pdf")
+    plt.show()
+    plt.close()
 
 
-    return data
-
-
-def _main(result_path: str, save_path: str):
+def _main(result_path: str, save_path: str, plot: bool = False):
     with open(result_path, "r") as file:
         data = json.load(file)
 
-    type = data["type"]
-
     data.pop("type", None)
 
-    if type == "singlelabel":
-        evaluation = singlelabel(data)
-    elif type == "multilabel":
-        evaluation = multilabel(data)
-    else:
-        evaluation = {}
+    evaluation = evaluate(data, save_path, plot)
 
-    evaluation["dataset_information"] = data["0"]["dataset_information"]
+    dataset_information = data["0"]["dataset_information"]
+    dataset_information.pop("random_state", None)
+
+    evaluation["dataset_information"] = dataset_information
     evaluation["number_of_datasets"] = len(data)
 
     with open("{}/evaluation.json".format(save_path), "w") as file:
@@ -91,7 +80,8 @@ if __name__ == "__main__":
                         help='Where the json which should be evaluated is saved.')
     parser.add_argument('-d', "--directory", action='store', type=str, default="multilabel_test_dataset",
                         help='Dictionary where to evaluation results.')
+    parser.add_argument("-p", "--plot", action='store_true', default=False, help='Whether to plot the results.')
 
     args = parser.parse_args()
 
-    _main(args.result_path, args.directory)
+    _main(args.result_path, args.directory, args.plot)
