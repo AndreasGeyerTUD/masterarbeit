4edba44bea725d7a23a3799be3d195b94c97dcb1
Andreas Geyer <andreasgeyer1@gmx.de>
2021-07-18 21:13:08 +0200

    - code adoption for using sns to plot the scatter matrix - scatter matrix is split when to many features - updated requirements.txt - updated plots for sklearn-datasets

--- existing_dataset_analysis/existing_dataset_analysis.py
+++ existing_dataset_analysis/existing_dataset_analysis.py
@@ -2,29 +2,68 @@ from sklearn.datasets import load_boston, load_iris, load_diabetes, load_digits,
     load_breast_cancer
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 datasets = [load_boston, load_iris, load_diabetes, load_wine, load_breast_cancer]
 
 see_what_is_possible = [load_digits, load_linnerud]
 
 
+def split_columns_for_plot(columns: list) -> list[list]:
+    if len(columns) > 10:
+        splitted_columns = []
+        split = int(len(columns) / 2)
+        first = columns[split:]
+        second = columns[:split]
+        if split > 10:
+            for spl in split_columns_for_plot(first):
+                splitted_columns.append(spl)
+            for spl in split_columns_for_plot(second):
+                splitted_columns.append(spl)
+        else:
+            splitted_columns.append(first)
+            splitted_columns.append(second)
+
+        return splitted_columns
+    else:
+        return [columns]
+
+
+def plot_scatter_matrix(data: pd.DataFrame, name: str):
+    sns.set_theme(style="white")
+
+    splits = split_columns_for_plot(data.columns)
+
+    for i in range(len(splits)):
+        for j in range(len(splits)):
+            g = sns.PairGrid(data, diag_sharey=False, x_vars=splits[i], y_vars=splits[j])
+            if i == j:
+                g.map_upper(sns.scatterplot)
+                g.map_lower(sns.kdeplot)
+                g.map_diag(sns.kdeplot)
+            elif i < j:
+                g.map(sns.kdeplot)
+            elif i > j:
+                g.map(sns.scatterplot)
+
+            g.fig.subplots_adjust(top=0.95)
+            g.fig.suptitle(name)
+
+            g.savefig("dataset_plots/{}_{}_{}.pdf".format(name, i, j), format="pdf")
+            plt.close()
+
+
 def sklearn_datasets():
     for dataset in datasets:
         ds = dataset()
         if "filename" in ds:
-            name = ds.filename.split("\\")[-1].split(".")[0]
+            name = ds.filename.split("\\")[-1].split("/")[-1].split(".")[0]
         else:
             name = str(dataset).split(" ")[1].split("_")[1]
 
         x = pd.DataFrame(ds.data, columns=ds.feature_names)
 
-        pd.plotting.scatter_matrix(x, diagonal="hist", figsize=(20, 20), alpha=1)
-
-        # x.plot(y=ds.target, kind="scatter", subplots=True)
-        plt.savefig("dataset_plots/{}.pdf".format(name), format="pdf")
-        plt.show()
-
-        print()
+        plot_scatter_matrix(x, name)
 
 
 if __name__ == "__main__":
